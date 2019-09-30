ff1d62b44e0fac9e780052a2c7f7150f337a8c9c
ZXMJ <1343421239@qq.com>
2019-09-30 17:52:05 +0800

    添加设计模式模块

---
+++ design_pattern/src/main/java/com/ultra/observer/ISubject.java
@@ -0,0 +1,25 @@
+package com.ultra.observer;
+
+public interface ISubject {
+
+    /**
+     * 订阅
+     *
+     * @param observer 观察者对象
+     */
+    void registerObserver(IObserver observer);
+
+    /**
+     * 取消
+     *
+     * @param observer 观察者对象
+     */
+    void removeObserver(IObserver observer);
+
+    /**
+     * 通知观察者
+     */
+    void notifyObservers();
+
+
+}
