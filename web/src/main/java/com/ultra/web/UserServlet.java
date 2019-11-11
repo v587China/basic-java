8dd08548f2be6095a8aa9532cb808fc965ad3324
ZXMJ <1343421239@qq.com>
2019-11-11 19:52:58 +0800

    cas单点

--- web/src/main/java/com/ultra/web/UserServlet.java
+++ web/src/main/java/com/ultra/web/UserServlet.java
@@ -2,6 +2,9 @@ package com.ultra.web;
 
 import javax.servlet.http.HttpServlet;
 
+/**
+ * @author admin
+ */
 public class UserServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
